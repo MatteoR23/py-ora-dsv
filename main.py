09b265cff170f6a79fb8fed926b29358317fea10
Matteo Rosada <matteorosada@gmail.com>
2021-11-13 01:06:04 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from settings import config
+import csv
+import cx_Oracle
+
+connString = f'{config["user"]}/{config["password"]}@{config["host"]}:{}/{config["service"]}'
+
+def getSqlFileContent(conf):
+    with open(conf["source_sql"], "r") as f:
+        return f.readlines()
+
+def generateCsvFromOracle(conf):
+    import os
+    sql = getSqlFileContent(conf)
+
+    con = cx_Oracle.connect(connString)
+    cursor = con.cursor()
+    csv_file = open(os.path.join(conf["path"], conf["outputfile"]), "w")
+    writer = csv.writer(csv_file, delimiter=conf["separator"], lineterminator=conf["lineTerminator"], quoting=csv.QUOTE_NONNUMERIC)
+    
+    r = cursor.execute(sql)
+    
+    for row in cursor:
+        writer.writerow(row)
+
+    cursor.close()
+    con.close()
+    csv_file.close()
+
+def main():
+    confArray = config["conf"]
+
+    for conf in confArray:
+        generateCsvFromOracle(conf)
+
+if __name__ == "__main__":
+    main()
